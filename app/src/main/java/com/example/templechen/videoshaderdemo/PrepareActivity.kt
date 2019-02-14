68072815886388a8101d9db7603bfbdd2b0e17a3
Wei Chen <weichen@narvii.com>
2019-02-14 11:17:53 +0800

    delete useless Activity

--- app/src/main/java/com/example/templechen/videoshaderdemo/PrepareActivity.kt
+++ app/src/main/java/com/example/templechen/videoshaderdemo/PrepareActivity.kt
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
 import com.example.templechen.videoshaderdemo.gl.SimpleGLActivity
-import com.example.templechen.videoshaderdemo.gl.editor.VideoEditorActivity
 import com.example.templechen.videoshaderdemo.gl.sticker.StickerActivity
 
 class PrepareActivity : AppCompatActivity(), View.OnClickListener {
@@ -14,7 +13,6 @@ class PrepareActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var recordBtn: Button
     private lateinit var playBtn: Button
     private lateinit var stickerBtn: Button
-    private lateinit var editorBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +24,6 @@ class PrepareActivity : AppCompatActivity(), View.OnClickListener {
         playBtn.setOnClickListener(this)
         stickerBtn.setOnClickListener(this)
 
-        editorBtn = findViewById(R.id.EditorBtn)
-        editorBtn.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -42,10 +38,6 @@ class PrepareActivity : AppCompatActivity(), View.OnClickListener {
             }
             R.id.stickerBtn -> {
                 val intent = Intent(this, StickerActivity::class.java)
-                startActivity(intent)
-            }
-            R.id.EditorBtn -> {
-                val intent = Intent(this, VideoEditorActivity::class.java)
                 startActivity(intent)
             }
         }
