b5978ea6619a37000553b4f06f15550060349561
Wei Chen <weichen@narvii.com>
2019-02-16 15:15:01 +0800

    optimize convertViewToBitmap performance

--- app/src/main/java/com/example/templechen/videoshaderdemo/gl/sticker/StickerUtil.kt
+++ app/src/main/java/com/example/templechen/videoshaderdemo/gl/sticker/StickerUtil.kt
@@ -1,7 +1,11 @@
 package com.example.templechen.videoshaderdemo.gl.sticker
 
 import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.util.Log
 import android.view.View
+
+
 
 class StickerUtil {
 
@@ -10,9 +14,14 @@ class StickerUtil {
         var bitmap: Bitmap? = null
 
         fun convertViewToBitmap(view: View): Bitmap {
-            view.isDrawingCacheEnabled = true
-            bitmap = Bitmap.createBitmap(view.getDrawingCache())
-            view.isDrawingCacheEnabled = false
+            val time = System.currentTimeMillis()
+//            view.isDrawingCacheEnabled = true
+//            bitmap = Bitmap.createBitmap(view.getDrawingCache())
+//            view.isDrawingCacheEnabled = false
+            bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_4444)
+            val canvas = Canvas(bitmap)
+            view.draw(canvas)
+//            Log.d("StickerUtil", (System.currentTimeMillis() - time).toString())
             return bitmap!!
         }
     }
